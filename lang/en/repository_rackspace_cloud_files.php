664978258edbb87d1e6f4246748ef7cdf9161aa3
Chris Brucks <cbrucks@gmail.com>
2013-01-23 12:16:12 -0600

    Create necessary files. Start building foundation code.

---
+++ lang/en/repository_rackspace_cloud_files.php
@@ -0,0 +1,33 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'repository_dropbox', language 'en', branch 'MOODLE_20_STABLE'
+ *
+ * @package   repository_rackspace_cloud_files
+ * @copyright 2013 Chris brucks
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['configplugin'] = 'Rackspace Cloud Files configuration';
+$string['notitle'] = 'notitle';
+$string['remember'] = 'Remember me';
+$string['pluginname'] = 'Rackspace Cloud Files';
+$string['apikey'] = 'Rackspace Cloud Files API key';
+$string['rackspace'] = 'Rackspace';
+$string['instruction'] = 'You can get your API Key from <a href="http://mycloud.rackspace.com">Rackspace Cloud Control Panel</a>. Expand the drop-down menu at the top right where your username is.  Click "API Keys" and finally click "Show Key"';
+$string['cdn'] = 'CDN (Content Delivery Network)';
+$string['cdn_info'] = 'CDN will cache files on servers closer to your users resulting in faster downloads of large files.';
---
+++ lib.php
@@ -0,0 +1,94 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * This plugin is used to access rackspace cloud files
+ *
+ * @since 2.0
+ * @package    repository_rackspace_cloud_files
+ * @copyright  2013 Chris Brucks
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+require_once($CFG->dirroot . '/repository/lib.php');
+require_once($CFG->dirroot . '/repository/cf.php');
+
+
+public class repository_rackspace_cloud_files extends repository {
+
+    function __constuct($repositoryid, $context = SYSCONTEXTID, $options = array()) {
+	    parent::__contstruct($repositoryid, $context, $options);
+	}
+
+    function get_listing() {
+        return array('list' => array());
+    }
+	
+	/**
+     * Generate search form
+     */
+    public function print_login($ajax = true) {
+        $ret = array();
+        $search = new stdClass();
+        $search->type = 'text';
+        $search->id   = 'youtube_search';
+        $search->name = 's';
+        $search->label = get_string('search', 'repository_youtube').': ';
+        $sort = new stdClass();
+        $sort->type = 'select';
+        $sort->options = array(
+            (object)array(
+                'value' => 'relevance',
+                'label' => get_string('sortrelevance', 'repository_youtube')
+            ),
+            (object)array(
+                'value' => 'published',
+                'label' => get_string('sortpublished', 'repository_youtube')
+            ),
+            (object)array(
+                'value' => 'rating',
+                'label' => get_string('sortrating', 'repository_youtube')
+            ),
+            (object)array(
+                'value' => 'viewCount',
+                'label' => get_string('sortviewcount', 'repository_youtube')
+            )
+        );
+        $sort->id = 'youtube_sort';
+        $sort->name = 'youtube_sort';
+        $sort->label = get_string('sortby', 'repository_youtube').': ';
+        $ret['login'] = array($search, $sort);
+        $ret['login_btn_label'] = get_string('search');
+        $ret['login_btn_action'] = 'search';
+        $ret['allowcaching'] = true; // indicates that login form can be cached in filepicker.js
+        return $ret;
+    }
+	
+	/**
+     * File types supported
+     * @return array
+     */
+    public function supported_filetypes() {
+        return array('*'); // Indicates that all types are supported
+    }
+
+    /**
+     * Plugin only return external links
+     * @return int
+     */
+    public function supported_returntypes() {
+        return FILE_EXTERNAL;
+    }
+}
