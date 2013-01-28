eba5831dd443d81cac39d7e3c9a70e19a06d27ab
Chris Brucks <cbrucks@gmail.com>
2013-01-28 15:46:41 -0600

    fix nav bar bug

--- lib.php
+++ lib.php
@@ -113,7 +113,7 @@ class repository_rackspace_cloud_files extends repository {
         $nav = array();
         $prev_path = '';
         foreach ($subfolders as $sub) {
-            $prev_path .= '/'.$sub;
+            $prev_path .= $sub.'/';
             $nav[] = array('name'=> $sub, 'path'=>$prev_path);
         }
 
@@ -141,7 +141,7 @@ class repository_rackspace_cloud_files extends repository {
         foreach($objects as $obj) {
             if (preg_match('/^[^.]+\.[a-zA-Z0-9]+$/', $obj->name)) {
                 // Add reference as a file
-                $dir_list[] = array('title'=>str_replace($path.'/', '', $obj->name), 'date'=>$obj->last_modified, 'size'=>$obj->content_length, 'source'=>$obj->public_uri(), 'url'=>$obj->public_uri(), 'thumbnail'=>$this->container->cdn_uri.'/'.$obj->name);
+                $dir_list[] = array('title'=>str_replace($path.'/', '', $obj->name), 'date'=>$obj->last_modified, 'size'=>$obj->content_length, 'source'=>$obj->public_uri(), 'url'=>$obj->public_uri(), 'thumbnail'=>$obj->public_uri());
             }
             else {
                 // Add reference as a folder
