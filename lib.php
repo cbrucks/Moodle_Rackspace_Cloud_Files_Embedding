2ba17d4221c5eb1272620e4d6d54b66aee58a1cb
Chris Brucks <cbrucks@gmail.com>
2013-01-28 10:49:42 -0600

    fix file and directory seperation

--- lib.php
+++ lib.php
@@ -132,7 +132,7 @@ class repository_rackspace_cloud_files extends repository {
         $files = array();
         
         foreach($objects as $obj) {
-            if (preg_match('~h~', $obj->name) {
+            if (preg_match('', $obj->name) {
                 $files[] = $obj;
             }
             else {
